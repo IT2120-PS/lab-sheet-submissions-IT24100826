664d542b2078c13a129d92017be5cf8f0091a2d6
IT24100826 <it24100826@my.sliit.lk>
2025-10-01 20:22:20 +0530

    Add files via upload

---
+++ Lab Sheet 09/IT24100826.R
@@ -0,0 +1,17 @@
+getwd()
+setwd("C:\\Users\\Aluthge Family Lap\\Desktop\\IT24100826")
+
+
+# Exercise 1
+# Part i
+
+baking_times <- rnorm(25, mean = 45, sd = 2)
+print(baking_times)
+
+
+# Part ii
+test_result <- t.test(baking_times, mu = 46, alternative = "less")
+
+print(test_result)
+
+
